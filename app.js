1b0f877c3b5a4250f3d8f02d6c5f033816adcc92
Marco2002 <marcoclone@gmail.com>
2018-01-29 15:23:36 +0000

    add basic /homework route setup

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 var express = require("express"),
 
 // Routes
-    indexRoutes = require("./routes/index");
+    indexRoutes = require("./routes/index"),
+    homeworkRoutes = require("./routes/homework");
 
 // express setup
 var app = express();
@@ -14,6 +15,7 @@ app.set("view engine", "ejs");
 
 // Routes setup
 app.use(indexRoutes);
+app.use("/homework", homeworkRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server online");
