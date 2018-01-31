c2e422b7c5fd8aff21c3f31cc2c2607c40137c06
Marco2002 <marcoclone@gmail.com>
2018-01-31 18:06:47 +0000

    Add warnings in the forms

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,4 +13,68 @@ $('.datepicker').pickadate({
     format: 'dd/mm/yy',
 });
 
-console.log("hello");
+// Form Configuration
+function formConfig() {
+    if(formConfigTitle() &&
+    formConfigDate() &&
+    formConfigAuthor() &&
+    formConfigDes() &&
+    formConfigSubject() == true) {
+        $("#form").submit();
+    } 
+}
+
+function formConfigTitle() {
+    var x = $("#homeworkTitle").val();
+    if (x == "") { 
+        Materialize.toast('Please enter a value for the title!', 4000);
+    } else if( x.length > 40 ) {
+        Materialize.toast('Title cant be longer than 40 characters!', 4000);
+    } else {
+        return true;
+    }
+}
+
+function formConfigDate() {
+    var x = $("#homeworkDate").val();
+    if(x == "") { 
+        Materialize.toast('Please enter a value for the Deadline!', 4000);
+    } else {
+        return true;
+    }
+}
+
+function formConfigAuthor() {
+    var x = $("#homeworkAuthor").val();
+    if (x == "") { 
+        Materialize.toast('Please enter a value for the Author!', 4000);
+    } else if( x.length > 20 ) {
+        Materialize.toast('The Authors Name cant be longer than 20 characters!', 4000);
+    } else {
+        return true;
+    }
+}
+
+function formConfigDes() {
+    var x = $("#homeworkDescription").val();
+    if (x == "") { 
+        Materialize.toast('Please enter a value for the Description!', 4000);
+    } else if( x.length > 300 ) {
+        Materialize.toast('Description cant be longer than 300 characters!', 4000);
+    } else {
+        return true;
+    }
+}
+
+function formConfigSubject() {
+    var x = $("#homeworkSubject").val();
+    if (x == "") { 
+        Materialize.toast('Please enter a value for the Subject!', 4000);
+    } else if( x.length > 20 ) {
+        Materialize.toast('Description cant be longer than 20 characters!', 4000);
+    } else {
+        return true;
+    }
+}
+
+console.log("script.js");
